958466ee159f6fdfc796f1cb9838e42412018089
Jade Jones <jadejonescrypto@gmail.com>
2023-01-03 12:20:06 +0000

    largest number test

--- B-write-tests/06-largest-number/largest-number.test.js
+++ B-write-tests/06-largest-number/largest-number.test.js
@@ -2,12 +2,17 @@ let getLargestNumber = require("./largest-number");
 
 test("returns largest number in array", function () {
   // Arrange
+  let numbers = [3, 21, 88, 4, 36]
   // Act
+  let expected = 88
   // Assert
+  let output = getLargestNumber(numbers)
+  expect(output).toEqual(expected);
 });
 
 // example
 // input: [3, 21, 88, 4, 36];
 // expected: 88;
+
 
 // also test that the original array hasn't changed
